871fe319e8359be10a23ca73afb7318eecc5d208
rahulgupta24 <rahulgupta24dec@gmail.com>
2023-10-03 21:33:17 +0530

    minor chanegs

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-console.log('process.env.MONGODB_URI', process.env.MONGODB_URI)
+//console.log('process.env.MONGODB_URI', process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
